db2771d6c6bde0c5b3d681280c8c77a2175fa830
Ragurandy <ragurandy5122@gmail.com>
2022-08-12 18:29:15 +0530

    ragu new

---
+++ src/com/For/ForLoop.java
@@ -0,0 +1,19 @@
+package com.For;
+
+import java.util.Iterator;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+               
+		for (int i = 0; i < 5; i++) {
+			
+			for (int j = 0; j <i-1; j++) {
+				System.out.print(i);
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
